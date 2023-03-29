687230dfc6a642288312de07150ecd5f3acfc9d2
Michael Kriese <michael.kriese@visualon.de>
2023-03-29 14:08:04 +0200

    feat: add another test

--- test.cjs
+++ test.cjs
@@ -2,10 +2,12 @@
 const assert = require('node:assert');
 const { test } = require('node:test');
 const fs = require('node:fs');
+const { dlopen, arch, platform } = require('node:process');
+const { constants } = require('node:os');
 
 test('uses right node arch and platform', () => {
-  assert.equal(process.platform, 'win32');
-  assert.equal(process.arch, 'x64');
+  assert.equal(platform, 'win32');
+  assert.equal(arch, 'x64');
 });
 
 test('module exists', () => {
@@ -15,6 +17,13 @@ test('module exists', () => {
   assert.ok(fs.existsSync(f));
 });
 
-test('loads module', () => {
+test('loads module with require', () => {
   assert.doesNotThrow(() => require('@swc/core-win32-x64-msvc'));
+});
+
+test('loads module with require', () => {
+  const module = { exports: {} };
+  const f = require.resolve('@swc/core-win32-x64-msvc');
+  dlopen(module, f, constants.dlopen.RTLD_NOW);
+  assert.ok(module.exports.minify);
 });
