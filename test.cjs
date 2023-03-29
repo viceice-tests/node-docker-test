48db0cbf414b9a5ec282d116adf4accc5ce4d003
Michael Kriese <michael.kriese@visualon.de>
2023-03-29 13:57:37 +0200

    feat: test node arch and platform

--- test.cjs
+++ test.cjs
@@ -3,6 +3,11 @@ const assert = require('node:assert');
 const { test } = require('node:test');
 const fs = require('node:fs');
 
+test('uses right node arch and platform', () => {
+  assert.equal(process.platform, 'win32');
+  assert.equal(process.arch, 'x64');
+});
+
 test('module exists', () => {
   const f = require.resolve('@swc/core-win32-x64-msvc');
   assert.ok(f);
