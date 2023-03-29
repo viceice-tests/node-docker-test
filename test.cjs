4fb4acba2340516770103f718a11f857ad7fa7df
Michael Kriese <michael.kriese@visualon.de>
2023-03-29 13:40:57 +0200

    fix: extend tests

--- test.cjs
+++ test.cjs
@@ -1,6 +1,14 @@
 'use strict';
 const assert = require('node:assert');
 const { test } = require('node:test');
+const fs = require('node:fs');
+
+test('module exists', () => {
+  const f = require.resolve('@swc/core-win32-x64-msvc');
+  assert.ok(f);
+  console.log(f);
+  assert.ok(fs.existsSync(f));
+});
 
 test('loads module', () => {
   assert.doesNotThrow(() => require('@swc/core-win32-x64-msvc'));
